6fd864515e5a07fbacc476ba32a332b24edd09bb
Natalia <zakrevskaya.97@inbox.ru>
2022-06-13 21:41:44 +0300

    fixed Thunks with redux-toolkit

--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -2,41 +2,53 @@ import { Dispatch } from 'redux';
 import { setAppStatusAC } from '../../app/app-reducer';
 import { authAPI, LoginParamsType } from '../../api/todolists-api';
 import { handleServerAppError, handleServerNetworkError } from '../../utils/error-utils';
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-const initialState = {
-  isLoggedIn: false,
+export const loginTC = createAsyncThunk( 'auth/login', async ( param: LoginParamsType, ThunkAPI ) => {
+  ThunkAPI.dispatch( setAppStatusAC( { status: 'loading' } ) );
+  try {
+    const res = await authAPI.login( param );
+    if ( res.data.resultCode === 0 ) {
+
+      ThunkAPI.dispatch( setAppStatusAC( { status: 'succeeded' } ) );
+      return { isLoggedIn: true };
+    } else {
+      handleServerAppError( res.data, ThunkAPI.dispatch );
+      return { isLoggedIn: false };
+    }
+  } catch ( error ) {
+    // @ts-ignore
+    handleServerNetworkError( error, ThunkAPI.dispatch );
+    return { isLoggedIn: false };
+  }
+} );
+
+export const loginTC_ = ( data: LoginParamsType ) => ( dispatch: Dispatch ) => {
+
 };
 
 const slice = createSlice( {
   name: 'auth',
-  initialState: initialState,
+  initialState: {
+    isLoggedIn: false,
+  },
   reducers: {
     setIsLoggedInAC( state, action: PayloadAction<{ value: boolean }> ) {
       state.isLoggedIn = action.payload.value;
     },
   },
+  extraReducers: builder => {
+    builder.addCase(loginTC.fulfilled, (state, action) => {
+      state.isLoggedIn = action.payload.isLoggedIn;
+    } )
+  }
 } );
 
 export const authReducer = slice.reducer;
 export const { setIsLoggedInAC } = slice.actions;
 
 // thunks
-export const loginTC = ( data: LoginParamsType ) => ( dispatch: Dispatch ) => {
-  dispatch( setAppStatusAC( { status: 'loading' } ) );
-  authAPI.login( data )
-    .then( res => {
-      if ( res.data.resultCode === 0 ) {
-        dispatch( setIsLoggedInAC( { value: true } ) );
-        dispatch( setAppStatusAC( { status: 'succeeded' } ) );
-      } else {
-        handleServerAppError( res.data, dispatch );
-      }
-    } )
-    .catch( ( error ) => {
-      handleServerNetworkError( error, dispatch );
-    } );
-};
+
 export const logoutTC = () => ( dispatch: Dispatch ) => {
   dispatch( setAppStatusAC( { status: 'loading' } ) );
   authAPI.logout()
--- src/features/TodolistsList/tasks-reducer.ts
+++ src/features/TodolistsList/tasks-reducer.ts
@@ -1,31 +1,29 @@
-import {
-  TaskPriorities,
-  TaskStatuses,
-  TaskType,
-  todolistsAPI,
-  TodolistType,
-  UpdateTaskModelType,
-} from '../../api/todolists-api';
+import { TaskPriorities, TaskStatuses, TaskType, todolistsAPI, UpdateTaskModelType } from '../../api/todolists-api';
 import { Dispatch } from 'redux';
 import { AppRootStateType } from '../../app/store';
 import { setAppStatusAC } from '../../app/app-reducer';
 import { handleServerAppError, handleServerNetworkError } from '../../utils/error-utils';
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { addTodolistAC, removeTodolistAC, setTodolistsAC } from './todolists-reducer';
 
 const initialState: TasksStateType = {};
+
+export const fetchTasksTC = createAsyncThunk( 'tasks/fetchTasks', async ( todolistId: string, ThunkAPI ) => {
+  ThunkAPI.dispatch( setAppStatusAC( { status: 'loading' } ) );
+  const res = await todolistsAPI.getTasks( todolistId );
+  const tasks = res.data.items;
+  ThunkAPI.dispatch( setAppStatusAC( { status: 'succeeded' } ) );
+  return { tasks, todolistId };
+} );
+export const removeTaskTC = createAsyncThunk( 'tasks/removeTask', async ( param: { taskId: string, todolistId: string }, ThunkAPI ) => {
+  const res = await todolistsAPI.deleteTask( param.todolistId, param.taskId );
+  return { taskId: param.taskId, todolistId: param.todolistId };
+} );
 
 export const slice = createSlice( {
   name: 'tasks',
   initialState: initialState,
   reducers: {
-    removeTaskAC( state, action: PayloadAction<{ taskId: string, todolistId: string }> ) {
-      const tasks = state[ action.payload.todolistId ];
-      const index = tasks.findIndex( task => task.id === action.payload.taskId );
-      if ( index > -1 ) {
-        tasks.splice( index, 1 );
-      }
-    },
     addTaskAC( state, action: PayloadAction<TaskType> ) {
       state[ action.payload.todoListId ].unshift( action.payload );
     },
@@ -35,9 +33,6 @@ export const slice = createSlice( {
       if ( index > -1 ) {
         tasks[ index ] = { ...tasks[ index ], ...action.payload.model };
       }
-    },
-    setTasksAC( state, action: PayloadAction<{ tasks: Array<TaskType>, todolistId: string }> ) {
-      state[ action.payload.todolistId ] = action.payload.tasks;
     },
   },
   extraReducers: ( builder ) => {
@@ -52,29 +47,24 @@ export const slice = createSlice( {
         state[ tl.id ] = [];
       } );
     } );
+    builder.addCase( fetchTasksTC.fulfilled, ( state, action ) => {
+      state[ action.payload.todolistId ] = action.payload.tasks;
+    } );
+    builder.addCase( removeTaskTC.fulfilled, ( state, action ) => {
+      const tasks = state[ action.payload.todolistId ];
+      const index = tasks.findIndex( task => task.id === action.payload.taskId );
+      if ( index > -1 ) {
+        tasks.splice( index, 1 );
+      }
+    } );
   },
 } );
 
 export const tasksReducer = slice.reducer;
-export const { removeTaskAC, addTaskAC, updateTaskAC, setTasksAC } = slice.actions;
+export const { addTaskAC, updateTaskAC } = slice.actions;
 
 // thunks
-export const fetchTasksTC = ( todolistId: string ) => ( dispatch: Dispatch ) => {
-  dispatch( setAppStatusAC( { status: 'loading' } ) );
-  todolistsAPI.getTasks( todolistId )
-    .then( ( res ) => {
-      const tasks = res.data.items;
-      dispatch( setTasksAC( { tasks, todolistId } ) );
-      dispatch( setAppStatusAC( { status: 'succeeded' } ) );
-    } );
-};
-export const removeTaskTC = ( taskId: string, todolistId: string ) => ( dispatch: Dispatch ) => {
-  todolistsAPI.deleteTask( todolistId, taskId )
-    .then( res => {
-      const action = removeTaskAC( { taskId, todolistId } );
-      dispatch( action );
-    } );
-};
+
 export const addTaskTC = ( title: string, todolistId: string ) => ( dispatch: Dispatch ) => {
   dispatch( setAppStatusAC( { status: 'loading' } ) );
   todolistsAPI.createTask( todolistId, title )
